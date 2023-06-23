6fd14369925c5d49874d5948f9ffe168e497820e
carolina <carito.antillanca@gmail.com>
2023-06-22 22:25:10 -0400

    corrección error bootstrap proveedor

--- proveedor-service/src/main/java/cl/tingeso/proveedorservice/ProveedorServiceApplication.java
+++ proveedor-service/src/main/java/cl/tingeso/proveedorservice/ProveedorServiceApplication.java
@@ -2,8 +2,12 @@ package cl.tingeso.proveedorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProveedorServiceApplication {
 
 	public static void main(String[] args) {
