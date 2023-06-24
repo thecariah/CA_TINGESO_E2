66d58d67b02adb9d913f8576212c5381d1f92043
carolina <carito.antillanca@gmail.com>
2023-06-23 22:38:27 -0400

    correccion import.sql acopio y grasol

--- acopio-service/src/main/resources/import.sql
+++ acopio-service/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO acopio(id,fecha,turno,proveedor,kls_leche) VALUES (0,"2023-03-19", "M", "01820", "24");
-INSERT INTO acopio(id,fecha,turno,proveedor,kls_leche) VALUES (1,"2023-03-20", "M", "01820", "5");
-INSERT INTO acopio(id,fecha,turno,proveedor,kls_leche) VALUES (2,"2023-03-19", "T", "02771", "10");
+INSERT INTO acopio(id,fecha,turno,proveedor,kls_leche) VALUES (0,'2023-03-19', 'M', '01820', '24');
+INSERT INTO acopio(id,fecha,turno,proveedor,kls_leche) VALUES (1,'2023-03-20', 'M', '01820', '5');
+INSERT INTO acopio(id,fecha,turno,proveedor,kls_leche) VALUES (2,'2023-03-19', 'T', '02771', '10');
