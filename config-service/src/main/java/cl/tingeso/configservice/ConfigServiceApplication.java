6c09c352a2549a3a8c45fd138b349e0d75ab9913
carolina <carito.antillanca@gmail.com>
2023-07-03 00:18:56 -0400

    config service, config data, eureka service inicializados. probando

---
+++ config-service/src/main/java/cl/tingeso/configservice/ConfigServiceApplication.java
@@ -0,0 +1,16 @@
+package cl.tingeso.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+
+}
